116d418e511bc5cd2029d2d034f0b2623014a7e8
tanmaykapse <kapsetanmay112@gmail.com>
2026-02-18 11:05:01 +0530

    commit

--- app/src/main/java/com/deepdefender/nagarsewahackthon/HomeActivity.java
+++ app/src/main/java/com/deepdefender/nagarsewahackthon/HomeActivity.java
@@ -2,23 +2,58 @@ package com.deepdefender.nagarsewahackthon;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.deepdefender.nagarsewahackthon.UserFragments.UserExploreFragment;
+import com.deepdefender.nagarsewahackthon.UserFragments.UserHistoryFragment;
+import com.deepdefender.nagarsewahackthon.UserFragments.UserHomeFragment;
+import com.deepdefender.nagarsewahackthon.UserFragments.UserProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        // Load default fragment
+        loadFragment(new UserHomeFragment());
+
+        bottomNavigation.setOnItemSelectedListener(item -> {
+
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_home) {
+                selectedFragment = new UserHomeFragment();
+            }
+            else if (item.getItemId() == R.id.nav_explore) {
+                selectedFragment = new UserExploreFragment();
+            }
+            else if (item.getItemId() == R.id.nav_history) {
+                selectedFragment = new UserHistoryFragment();
+            }
+            else if (item.getItemId() == R.id.nav_profile) {
+                selectedFragment = new UserProfileFragment();
+            }
+
+            return loadFragment(selectedFragment);
         });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainer, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
